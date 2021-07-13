7fa3a90f08b153555e1bde21adfa541f27dd9137
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-07-14 00:07:16 +0530

    creating basic setups

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -5,7 +5,11 @@ from django.http import HttpResponse
 def projects(request):
     return HttpResponse('This is out projects page.')
 
+def project(request, pk):
+    return HttpResponse(f'This is project page. Id send is : {pk}')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', projects),
+    path('project/<str:pk>/', project),
 ]
