eb1c69c075f2316d67083854a3c28d9f2225c453
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-07-14 00:41:20 +0530

    updated vs code settings for formatting and script

--- projects/views.py
+++ projects/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render, HttpResponse
 def projects(request):
     return HttpResponse('This is out projects page.')
 
+
 def project(request, pk):
     return HttpResponse(f'This is project page. Id send is : {pk}')
