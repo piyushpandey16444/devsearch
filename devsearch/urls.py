aadb2e134ae8d8580b82e0164637fbdd1b915a76
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-07-14 00:03:52 +0530

    views created

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def projects(request):
+    return HttpResponse('This is out projects page.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('projects/', projects),
 ]
