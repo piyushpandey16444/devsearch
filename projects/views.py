fe1acb3dc3a2151d1c2b647482e4f41c2025c0e4
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-07-14 10:24:10 +0530

    added templates

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 
 def projects(request):
-    return HttpResponse('This is out projects page.')
+    return render(request, 'projects.html')
 
 
 def project(request, pk):
-    return HttpResponse(f'This is project page. Id send is : {pk}')
+    return render(request, 'project.html')
