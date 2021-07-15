9457eb85169302c9e69ed8fda29730a6f4313f13
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-07-15 22:45:07 +0530

    sent data dynamically

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
 
+projectsList = [
+    {
+        'id': '1',
+        'title': 'Ecommerce Website',
+        'description': 'Fully functional ecommerce website',
+        'toprated': True
+    },
+    {
+        'id': '2',
+        'title': 'Portfolio Website',
+        'description': 'A personal website to write articles and display work',
+        'toprated': True
+    },
+    {
+        'id': '3',
+        'title': 'Social Network',
+        'description': 'An open source project built by the community',
+        'toprated': False
+    }
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    context = {"list_data": projectsList}
+    return render(request, 'projects/projects.html', context=context)
 
 
 def project(request, pk):
-    return render(request, 'projects/project.html')
+    project_objs = None
+    for dt in projectsList:
+        if dt.get('id') == str(pk):
+            project_objs = dt
+    return render(request, 'projects/project.html', context={'project_objs': project_objs})
